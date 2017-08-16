d8c2ca46a145bafca793908c5e364863f1bf1f8d
yujimk <18862493252@163.com>
2017-08-16 22:26:02 +0800

    push

---
+++ TxtReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var reader = new TxtReader();
+                reader.FileStremRead(@"G:\TestData\first.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
